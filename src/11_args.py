e0deafe1272314d528d6fa5a342ffeecf879075e
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-01 19:44:21 -0400

    Completed file 11

--- src/11_args.py
+++ src/11_args.py
@@ -1,3 +1,4 @@
+import collections.abc
 # Experiment with positional arguments, arbitrary arguments, and keyword
 # arguments.
 
@@ -5,6 +6,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -13,6 +16,17 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    x = 0
+
+    for arg in args:
+        if isinstance(arg, collections.Iterable):
+            for item in arg:
+                x = x + item
+        else:
+            x = x + arg
+
+    return x
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -30,10 +44,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y = 1):
+    return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
-
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
@@ -44,6 +59,15 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*args, **kwargs):
+    for arg in args:
+        if isinstance(arg, collections.Iterable):
+            for key, value in arg.items():
+                print(f"{key}: {value}")
+        else:
+            print(arg)
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
 
 # Should print
 # key: a, value: 12
